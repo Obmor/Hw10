1a6203d46e47c65c55fd8f0f8ad6ad484f6e415a
Obmor <terekhin.alex@mail.ru>
2024-09-16 14:49:28 +0300

    TheFirstLook

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Exercise 1.");
+        String firstName = "Семён";
+        String middleName = "Семёнович";
+        String lastName = "Иванов";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println();
 
+
+        System.out.println("Exercise 2.");
+        String fullNameUpperCase = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — "
+                + fullNameUpperCase);
+        System.out.println();
+
+
+        System.out.println("Exercise 3.");
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName);
+        System.out.println();
     }
 }
